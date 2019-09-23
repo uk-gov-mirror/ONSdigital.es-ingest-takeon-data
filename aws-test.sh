925c3567ab07e8b4f860c3354d1c84ee0cae5e25
“thomashenson” <thomasjameshenson@gmail.com>
2019-09-23 16:55:24 +0100

    Changed env vars in serverless yml and fixed test script.

--- aws-test.sh
+++ aws-test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# set -e
+set -e
 
 # Run flake8 linting
-flake8 GIT-repository 
-
-# && echo "flake8 passed. Yayyy." || exit 1
+flake8 GIT-repository && echo "flake8 passed. Yayyy." || exit 1
 
 # Run unit tests
 pytest GIT-repository
