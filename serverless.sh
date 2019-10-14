31f1ba7ab1a44e5584d3db83dca2e915a07d320b
Jordancooke <Jordanleecooke@gmail.com>
2019-10-14 14:18:19 +0100

    Updating references to json

--- serverless.sh
+++ serverless.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-cd subnets
-ls
-cat subnets_output.json
-cd ../
+
 cd deploy-repository
 
 echo Packaging serverless bundle...
+
 serverless package --package pkg
 serverless deploy --verbose;
