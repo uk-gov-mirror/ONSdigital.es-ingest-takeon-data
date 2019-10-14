be32745a972ad846437d412b926f8ac08d1d41dd
Jordancooke <Jordanleecooke@gmail.com>
2019-10-14 13:32:22 +0100

    Configuration of serverless to get variables working

--- serverless.sh
+++ serverless.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
+cd subnets
+ls
+cat subnets.json
+cd ../
 cd deploy-repository
 
 echo Packaging serverless bundle...
 serverless package --package pkg
-mv pkg ../
-cd ../
-ls
-ls subnets
 serverless deploy --verbose;
