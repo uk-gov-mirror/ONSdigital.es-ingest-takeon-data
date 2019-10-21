cd189adeb22fc6b0f90fdb2a292ab5fa5ef6a06e
“thomashenson” <thomasjameshenson@gmail.com>
2019-10-21 10:39:29 +0100

    Fixing errors in Serverless
    Fixed issue where serverless script file was cd'ing into a non-existant directory
    
    Amended serverless yml to pull correct subnet id's instead of vpc id's

--- serverless.sh
+++ serverless.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd deploy-repository
 
+cd ingest-deploy-repository
 echo Packaging serverless bundle...
-
 serverless package --package pkg
+echo Deploying to AWS...
 serverless deploy --verbose;
