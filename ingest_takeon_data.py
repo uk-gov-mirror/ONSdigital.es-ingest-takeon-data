668c38db2e84e4fb87539bd11094426a3eef1070
David Lees <david@fides.me.uk>
2019-11-08 13:45:57 +0000

    Tidy Up.

--- ingest_takeon_data.py
+++ ingest_takeon_data.py
@@ -14,15 +14,15 @@ class InputSchema(Schema):
     These vairables are expected by the method, and it will fail to run if not provided.
     :return: None
     """
-    takeon_bucket_name = fields.Str(required=True)
-    results_bucket_name = fields.Str(required=True)
-    function_name = fields.Str(required=True)
     checkpoint = fields.Str(required=True)
-    sns_topic_arn = fields.Str(required=True)
     in_file_name = fields.Str(required=True)
+    method_name = fields.Str(required=True)
     out_file_name = fields.Str(required=True)
     queue_url = fields.Str(required=True)
+    results_bucket_name = fields.Str(required=True)
+    sns_topic_arn = fields.Str(required=True)
     sqs_messageid_name = fields.Str(required=True)
+    takeon_bucket_name = fields.Str(required=True)
 
 
 def lambda_handler(event, context):
@@ -45,15 +45,15 @@ def lambda_handler(event, context):
 
         # ENV vars
         config, errors = InputSchema().load(os.environ)
-        takeon_bucket_name = config['takeon_bucket_name']
-        results_bucket_name = config['results_bucket_name']
-        in_file_name = config['in_file_name']
-        function_name = config['function_name']
         checkpoint = config['checkpoint']
-        sns_topic_arn = config['sns_topic_arn']
-        out_file_name = config['out_file_name']  # "test_results_ingest_output.json"
+        in_file_name = config['in_file_name']
+        method_name = config['method_name']
+        out_file_name = config['out_file_name']
         queue_url = config['queue_url']
+        results_bucket_name = config['results_bucket_name']
+        sns_topic_arn = config['sns_topic_arn']
         sqs_messageid_name = config['sqs_messageid_name']
+        takeon_bucket_name = config['takeon_bucket_name']
 
         logger.info("Validated environment parameters.")
 
@@ -62,13 +62,13 @@ def lambda_handler(event, context):
         logger.info("Read from S3.")
 
         method_return = lambda_client.invoke(
-         FunctionName=function_name, Payload=input_file
+         FunctionName=method_name, Payload=input_file
         )
 
-        output_json = method_return.get('Payload').read().decode("utf-8")
+        output_json = json.loads(method_return.get('Payload').read().decode("utf-8"))
 
         funk.save_data(results_bucket_name, out_file_name,
-                       json.loads(output_json), queue_url, sqs_messageid_name)
+                       output_json, queue_url, sqs_messageid_name)
 
         logger.info("Data ready for Results pipeline. Written to S3.")
 
