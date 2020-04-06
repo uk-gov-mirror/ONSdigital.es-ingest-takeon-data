de1774b2c4c0c04cf088bf4e75ec0041000bb235
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-04-06 13:01:51 +0100

    Update serverless.sh

--- serverless.sh
+++ serverless.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd ingest-deploy-repository
+echo Installing dependencies
 serverless plugin install --name serverless-pseudo-parameters
 serverless plugin install --name serverless-latest-layer-version
 echo Packaging serverless bundle...
